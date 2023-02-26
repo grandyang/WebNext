2cdcf9e27695375f5579f99f33211c1813f4e35e
Jie Mei <jimei@microsoft.com>
2023-02-26 12:32:14 -0800

    Update transaction logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,14 @@ import item_channel_3 from './img/Channel-item-3.jpg'
 import item_channel_4 from './img/Channel-item-4.jpg'
 import item_channel_5 from './img/Channel-item-5.jpg'
 
+const baseUrl = "http://172.29.170.179:5000";
+// const baseUrl = "http://127.0.0.1:5000";
+const nftAmount = 1;
+
+var seller = {};
+var creator = {};
+var bidder = {};
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
@@ -34,9 +42,21 @@ const Item = styled(Paper)(({ theme }) => ({
 
 function App() {
   const [accountsStatus, setAccountsStatus] = useState('Not Started.');
-  const [aliceAddress, setAliceAddress] = useState('');
-  const [bobAddress, setBobAddress] = useState('');
-  const [carlaAddress, setCarlaAddress] = useState('');
+  const [nftStatus, setNftStatus] = useState("Not Started.");
+  const [transactionStatus, setTransactionStatus] = useState("Not Started.");
+  const [aliceAccount, setAliceAccount] = useState("");
+  const [aliceBalance, setAliceBalance] = useState("");
+  const [aliceBalance2, setAliceBalance2] = useState("");
+  const [aliceBalance3, setAliceBalance3] = useState("");
+  const [bobAccount, setBobAccount] = useState("");
+  const [bobBalance, setBobBalance] = useState("");
+  const [bobBalance2, setBobBalance2] = useState("");
+  const [bobBalance3, setBobBalance3] = useState("");
+  const [carlaAccount, setCarlaAccount] = useState("");
+  const [carlaBalance, setCarlaBalance] = useState("");
+  const [carlaBalance2, setCarlaBalance2] = useState("");
+  const [carlaBalance3, setCarlaBalance3] = useState("");
+  const [nftId, setNftId] = useState("");
 
     // useEffect(() => {
   //    fetch('http://127.0.0.1:5000/accounts')
@@ -49,22 +69,146 @@ function App() {
   //          console.log(err.message);
   //       }));
   // }, []);
+    async function createAccounts() {
+    console.log("createAccounts");
+    setAccountsStatus("Creating Accounts");
+    await fetch(`${baseUrl}/auction/start`).then((response) =>
+      response
+        .json()
+        .then((data) => {
+          console.log(data);
+          console.log("here 1");
+          setAccountsStatus("Accounts created");
+          setAliceAccount(`${data.seller}`);
+          setBobAccount(`${data.creator}`);
+          setCarlaAccount(`${data.bidder}`);
 
-  function addToList() {
-      console.log("Adding to list");
-      setAccountsStatus("Creating Accounts");
-      fetch('http://172.29.170.179:5000/auction/start')
-          .then((response) => response.json()
-              .then((data) => {
-                    console.log(data);
-                    setAccountsStatus("Accounts created");
-                    setAliceAddress(data.seller);
-                    setBobAddress(data.creator);
-                    setCarlaAddress(data.bidder);
-              })
-              .catch((err) => {
-                  console.log(err.message);
-              }));
+          seller = `${data.seller}`;
+          creator = `${data.creator}`;
+          bidder = `${data.bidder}`;
+        })
+        .catch((err) => {
+          console.log(err.message);
+        })
+    );
+
+    console.log(aliceAccount)
+    console.log(bobAccount)
+    console.log(carlaAccount)
+
+    console.log(seller);
+    console.log(creator);
+    console.log(bidder);
+
+    getAllAccountsBalance(1);
+  }
+
+  async function getAllAccountsBalance(cnt=0) {
+
+    console.log(aliceAccount)
+    console.log(bobAccount)
+    console.log(carlaAccount)
+
+    console.log(seller);
+    console.log(creator);
+    console.log(bidder);
+
+    await fetch(`${baseUrl}/account_balance`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        seller: seller,
+        creator: creator,
+        bidder: bidder,
+      }),
+    }).then((response) =>
+      response
+        .json()
+        .then((data) => {
+          console.log(data);
+
+          if (cnt === 2) {
+            console.log(cnt);
+            setAliceBalance2(data.seller);
+            setBobBalance2(data.creator);
+            setCarlaBalance2(data.bidder);
+          } else if (cnt === 3) {
+            console.log(cnt);
+            setAliceBalance3(data.seller);
+            setBobBalance3(data.creator);
+            setCarlaBalance3(data.bidder);
+          } else {
+            console.log(cnt);
+            setAliceBalance(data.seller);
+            setBobBalance(data.creator);
+            setCarlaBalance(data.bidder);
+          }
+        })
+        .catch((err) => {
+          console.log(err.message);
+        })
+    );
+  }
+
+  async function createNFT() {
+    console.log("createNFT");
+    setNftStatus("Creating NFT");
+
+    await fetch(`${baseUrl}/auction/nft/${nftAmount}`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: aliceAccount,
+    }).then((response) =>
+      response
+        .json()
+        .then((data) => {
+          console.log(data);
+          setNftStatus(`NFT created, nftId is '${data.nftID}'`);
+          setNftId(`${data.nftID}`);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        })
+    );
+
+    await getAllAccountsBalance(2);
+  }
+
+  async function createTransaction() {
+
+    console.log("createTransaction");
+    setTransactionStatus("Creating Transaction");
+
+    await fetch(`${baseUrl}/transaction/${nftId}/${nftAmount}`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        seller: aliceAccount,
+        creator: bobAccount,
+        bidder: carlaAccount,
+      }),
+    }).then((response) =>
+      response
+        .json()
+        .then((data) => {
+          console.log(data);
+          setTransactionStatus(`Transaction completed`);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        })
+    );
+
+    await getAllAccountsBalance(3);
   }
 
   function purchase() {
@@ -75,9 +219,6 @@ function App() {
               .then((data) => {
                     console.log(data);
                     setAccountsStatus("Accounts created");
-                    setAliceAddress(data.seller);
-                    setBobAddress(data.creator);
-                    setCarlaAddress(data.bidder);
               })
               .catch((err) => {
                   console.log(err.message);
@@ -88,7 +229,7 @@ function App() {
   return new Promise((resolve) => setTimeout(resolve, 2000));
 }
 
-  function LoadingButton() {
+  async function LoadingButton() {
     const [isLoading, setLoading] = useState(false);
     const [isPurchased, setPurchased] = useState(false);
 
@@ -101,6 +242,9 @@ function App() {
     }, [isLoading]);
 
     const handleClick = () => {
+      createAccounts();
+      createNFT();
+      createTransaction();
       setLoading(true);
       setPurchased(true);
     }
@@ -227,7 +371,7 @@ function App() {
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="light" onClick={addToList}>
+            <Button variant="light" onClick={createNFT}>
               Add to Watch List
             </Button>
             <LoadingButton />
